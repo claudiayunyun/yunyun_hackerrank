c7433155008e8625706bf9ce83980c2ae1875b01
Yunyun Huang <claudiayunyun@gmail.com>
2018-09-12 13:28:15 -0700

    find the second largest number, sys.maxsize for max int

--- python/RunnerupScore.py
+++ python/RunnerupScore.py
@@ -1,9 +1,10 @@
 # find the second largest number in the list, which is not equal to the largest
+import sys
 if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
-    max = 0
-    second_max = 0
+    max = - sys.maxsize
+    second_max = - sys.maxsize
     for score in arr:
         if score > max:
             second_max = max
