857133b46409511d5bc582c74247f9c3a9515fd3
Yunyun Huang <claudiayunyun@gmail.com>
2018-09-21 16:03:29 -0700

    input function

--- python/InputFunction.py
+++ python/InputFunction.py
@@ -2,6 +2,10 @@
 # Built-in functions, The input() function allows user input.
 # input(prompt) prompt	A String, representing a default message before the input.
 
+def print_full_name(a, b):
+    print('Hello ' + a + ' ' + b + '! You just delved into python.')
+
+
 if __name__ == '__main__':
     x = input('Enter your name:')
     print('Hello, ' + x)
@@ -9,3 +13,7 @@ if __name__ == '__main__':
     print('Enter your name:')
     y = input()
     print('Hello, ' + y)
+
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
