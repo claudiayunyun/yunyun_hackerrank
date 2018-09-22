d7e7e1bd10b191aa72b2420b3d92fb42f3d6eccd
Yunyun Huang <claudiayunyun@gmail.com>
2018-09-22 13:21:42 -0700

    rjust(), ljust(), center() for text align

---
+++ python/TextAlignment.py
@@ -0,0 +1,27 @@
+# ljust(), rjust(), center()
+# syntax : string.ljust(length, character)
+# length	Required. The length of the returned string
+# character	Optional. A character to fill the missing space (to the right of the string). Default is " " (space).
+
+thickness = int(input()) #This must be an odd number, 5 for test
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
