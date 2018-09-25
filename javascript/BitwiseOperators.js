2836c0d1c1a7b885ee9de9a76029e53d1089e7d1
Yunyun Huang <claudiayunyun@gmail.com>
2018-09-25 15:18:54 -0700

    bitwise operators, (i&j) > max, i&j > max, compare sequence not same

--- javascript/BitwiseOperators.js
+++ javascript/BitwiseOperators.js
@@ -1,27 +1,13 @@
-let result = 0;
-let size = Math.ceil(Math.log(n));
-console.log(size)
-for(let a = 1; a <= n; a++) {
-    for(let b = a + 1; b <= n; b++) {
-        let bitA = [];
-        let bitB = [];
-        for(let i = 0; i < size; i++ ) {
-            bitA.push(a % 2);
-            a = a / 2;
-            bitB.push(b % 2);
-            b = b / 2;
-        }
-        let bitAnd = [];
-        let tenBase = 0;
-        for(let i = 0; i < bitA.length; i++) {
-            bitAnd.push(bitA[i] || bitB[i]);
-        }
-        for(let bit of bitAnd) {
-            tenBase = tenBase * 2 + bit; 
-        }
-        if (tenBase > result && tenBase < k) {
-            result = tenBase;
+function getMaxLessThanK(n, k) {
+    let max = -1;
+    for(let i = 1; i <= n; i++) {
+        for(let j = 1; j < i; j++) {
+            if((i&j) > max && (i&j) < k) {
+                max = i&j;
+            }
         }
     }
+    return max;
 }
-return result;
+
+console.log(getMaxLessThanK(2, 2));
